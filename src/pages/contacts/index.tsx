22dead51e0a30ea3528c248ef71c03cf60d174e3
Alex Margaryan <margryanax@gmail.com>
2022-08-22 14:31:07 +0400

    full

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -9,50 +9,54 @@ const Contacts = () => {
     (window.open(url, "_blank") as Window).focus();
   };
   return (
-    <div className="P-contacts">
-      <h2>{t("Contact-text")}</h2>
-      <h3>{t("We_Are_Here-text")}</h3>
-      <p>{t("Ready_to_help-text")}</p>
+    <div className="P-contacts ">
+      <div className="G-container">
+        <h2>{t("Contact-text")}</h2>
+        <h3>{t("We_Are_Here-text")}</h3>
+        <p>{t("Ready_to_help-text")}</p>
+      </div>
       <div className="P-divider" />
-      <h3>{t("Lets_Get_Started-text")}</h3>
-      <p>{t("Don't_hesitate-text")}</p>
-      <p>
-        {t("Or_send-text")}{" "}
-        <span
-          onClick={() => {
-            window.location = "mailto:info@hawx.io" as (string | Location) &
-              Location;
+      <div className="G-container">
+        <h3>{t("Lets_Get_Started-text")}</h3>
+        <p>{t("Don't_hesitate-text")}</p>
+        <p>
+          {t("Or_send-text")}{" "}
+          <span
+            onClick={() => {
+              window.location = "mailto:info@hawx.io" as (string | Location) &
+                Location;
 
-            navigator.clipboard.writeText("info@hawx.io ");
-          }}
-        >
-          info@hawx.io
-        </span>{" "}
-        {t("and_our_professional-text")}
-      </p>
-      <h4>{t("FOLLOW-text")}</h4>
-      <div className="G-flex P-icons">
-        <img
-          src={fb}
-          alt="facebook"
-          onClick={() => {
-            goTo("https://www.facebook.com/HawX.io");
-          }}
-        />
-        <img
-          src={linkedin}
-          alt="linkedin"
-          onClick={() => {
-            goTo("https://www.linkedin.com/company/hawx/");
-          }}
-        />
-        <img
-          src={inst}
-          alt="instagram"
-          onClick={() => {
-            goTo("https://www.instagram.com/hawx.io/");
-          }}
-        />
+              navigator.clipboard.writeText("info@hawx.io ");
+            }}
+          >
+            info@hawx.io
+          </span>{" "}
+          {t("and_our_professional-text")}
+        </p>
+        <h4>{t("FOLLOW-text")}</h4>
+        <div className="G-flex P-icons">
+          <img
+            src={fb}
+            alt="facebook"
+            onClick={() => {
+              goTo("https://www.facebook.com/HawX.io");
+            }}
+          />
+          <img
+            src={linkedin}
+            alt="linkedin"
+            onClick={() => {
+              goTo("https://www.linkedin.com/company/hawx/");
+            }}
+          />
+          <img
+            src={inst}
+            alt="instagram"
+            onClick={() => {
+              goTo("https://www.instagram.com/hawx.io/");
+            }}
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -2,9 +2,31 @@ import { useTranslation } from "react-i18next";
 import NewsCard from "src/components/newsCard";
 import "./style.scss";
 import jsonFile from "src/i18n/locales/en/translations.json";
+import { useEffect, useState } from "react";
 
 const News = () => {
   const { t } = useTranslation();
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [count, setCount] = useState<number>(4);
+  useEffect(() => {
+    window.addEventListener("resize", (e: Event) => {
+      setWidth(window.innerWidth);
+    });
+    window.addEventListener("scroll", (e: any) => {
+      if (
+        e.target.scrollingElement.scrollHeight -
+          e.target.scrollingElement.clientHeight -
+          e.target.scrollingElement.scrollWidth <
+        e.target.scrollingElement.scrollTop
+      ) {
+        setCount(count + 4);
+      }
+    });
+    return () => {
+      window.removeEventListener("resize", (e: Event) => {});
+      window.removeEventListener("scroll", (e: Event) => {});
+    };
+  }, []);
   return (
     <div className="P-news">
       <div className="G-container">
@@ -14,15 +36,19 @@ const News = () => {
         <h3>{t("LASTEST-text")}</h3>
         <div className="P-news-container">
           {Object.values(jsonFile.news).map((item, index) => {
-            return (
-              <div className="P-card" key={index}>
-                <NewsCard
-                  img={item.img}
-                  title={t(`news.news${index}.newsTitle`)}
-                  content={t(`news.news${index}.newsContent`)}
-                />
-              </div>
-            );
+            if (width > 582 || index < count) {
+              return (
+                <div className="P-card" key={index}>
+                  <NewsCard
+                    img={item.img}
+                    title={t(`news.news${index}.newsTitle`)}
+                    content={t(`news.news${index}.newsContent`)}
+                  />
+                </div>
+              );
+            } else {
+              return null;
+            }
           })}
         </div>
       </div>
